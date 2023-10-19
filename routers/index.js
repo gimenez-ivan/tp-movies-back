d5c12163f08c4185fbacc2d844f24a8b2e37e44a
Nicokevo <82186948+Nicokevo@users.noreply.github.com>
2023-10-18 22:39:09 -0300

    Router index

---
+++ routers/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+
+const router = Router();
+
+const usuariosRouter = require('./usuarios.router');
+const peliculasRouter = require('./peliculas.router');
+
+router.use('/usuarios', usuariosRouter);
+router.use('/peliculas', peliculasRouter);
+
+module.exports = router;
